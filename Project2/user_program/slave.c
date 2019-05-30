e9d5631f75724f93f601cfd49ffca5acc74ab65a
perry0513 <perrypeiweichen@gmail.com>
2019-05-30 18:54:07 +0800

    add comments

--- Project2/user_program/slave.c
+++ Project2/user_program/slave.c
@@ -14,7 +14,7 @@
 int main (int argc, char* argv[])
 {
 	char buf[BUF_SIZE];
-	int i, dev_fd, file_fd;// the fd for the device and the fd for the input file
+	int i, dev_fd, file_fd; // the fd for the device and the fd for the input file
 	size_t ret, file_size = 0, data_size = -1;
 	char file_name[50];
 	char method[20];
@@ -29,11 +29,13 @@ int main (int argc, char* argv[])
 	strcpy(method, argv[2]);
 	strcpy(ip, argv[3]);
 
-	if( (dev_fd = open("/dev/slave_device", O_RDWR)) < 0)//should be O_RDWR for PROT_WRITE when mmap()
+	// should be O_RDWR for PROT_WRITE when mmap()
+	if( (dev_fd = open("/dev/slave_device", O_RDWR)) < 0)
 	{
 		perror("failed to open /dev/slave_device\n");
 		return 1;
 	}
+	// time when the device is opened
 	gettimeofday(&start ,NULL);
 	if( (file_fd = open (file_name, O_RDWR | O_CREAT | O_TRUNC)) < 0)
 	{
@@ -41,34 +43,44 @@ int main (int argc, char* argv[])
 		return 1;
 	}
 
-	if(ioctl(dev_fd, 0x12345677, ip) == -1)	//0x12345677 : connect to master in the device
+	// 0x12345677 : connect to master in the device
+	// 0x12345677 : master_IOCTL_CREATESOCK in master_device.c
+	if(ioctl(dev_fd, 0x12345677, ip) == -1)
 	{
 		perror("ioclt create slave socket error\n");
 		return 1;
 	}
 
+	// write "ioctl success\n" to stdout (fd = 1)
     write(1, "ioctl success\n", 14);
 
 	switch(method[0])
 	{
-		case 'f'://fcntl : read()/write()
+		case 'f': // fcntl : read()/write()
 			do
 			{
-				ret = read(dev_fd, buf, sizeof(buf)); // read from the the device
-				write(file_fd, buf, ret); //write to the input file
+				// read from the device, ret is the number of bytes read
+				ret = read(dev_fd, buf, sizeof(buf));
+				// write the number of bytes read to the file
+				write(file_fd, buf, ret);
+				// increment file_size
 				file_size += ret;
-			}while(ret > 0);
+			} while(ret > 0);
 			break;
 	}
 
 
-
-	if(ioctl(dev_fd, 0x12345679) == -1)// end receiving data, close the connection
+	// end receiving data, close the connection
+	// 0x12345679 : master_IOCTL_EXIT in master_device.c
+	if(ioctl(dev_fd, 0x12345679) == -1)
 	{
 		perror("ioclt client exits error\n");
 		return 1;
 	}
+	// time when device is closed
 	gettimeofday(&end, NULL);
+
+	// calculate time
 	trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)*0.0001;
 	printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, file_size / 8);
 
